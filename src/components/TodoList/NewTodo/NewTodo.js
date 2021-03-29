06a9b7fa8e443d717645873cf1e67fa18ba8d862
Akshay Sutar <akshay.sutar@plus91.in>
2021-03-29 21:21:25 +0530

    fixed issue in new todo item

--- src/components/TodoList/NewTodo/NewTodo.js
+++ src/components/TodoList/NewTodo/NewTodo.js
@@ -4,7 +4,7 @@ const NewTodo = (props) => {
     return (
         <div className="new-todo" >
             <input type="text" placeholder="Type title" value={props.title} onChange={props.titleChanged} />
-            <textarea placeholder="Type description" value={props.description} onChange={props.handleNewDescriptionChangeEvent} ></textarea>
+            <textarea placeholder="Type description" value={props.description} onChange={props.descriptionChanged} ></textarea>
             <button onClick={props.clicked} >Create</button>
         </div>
     );
